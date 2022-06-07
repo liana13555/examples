7d03d07912f4d136a4fbfa4d3fd3539f41727b1e
liana13555 <barishovaliana@gmail.com>
2022-06-07 21:59:04 +0200

    Update files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const spzInput = document.querySelector(".spzInput");
-const spzInfo = document.querySelector(".spzInfo");
+const licenceInput = document.querySelector(".licenceInput");
+const licenceInfo = document.querySelector(".licenceInfo");
 const validInfo = document.querySelector(".validInfo");
-const spzCode = document.querySelector(".spzCode");
+const licenceCode = document.querySelector(".licenceCode");
 const shownSPZ = document.querySelector(".inputLicence");
 const submitBtn = document.querySelector(".button");
 const msgBox = document.querySelector(".msgBox");
@@ -9,31 +9,36 @@ const msgBox = document.querySelector(".msgBox");
 const spzFilter = /\d[A-C,E,H,J-M,P,S-U,Z][A-Z0-9]\d{4}/;
 const regionFilter = /\d[A-C,E,H,J-M,P,S-U,Z][A-Z0-9]/;
 
-spzInput.addEventListener("input", e => {
+licenceInput.addEventListener("focus", e => {
+    msgBox.innerHTML = "";
+    licenceInfo.style.display = "block";
+})
+
+licenceInput.addEventListener("input", e => {
     msgBox.innerHTML = "";
     const value = e.currentTarget.value;
-    
+
     shownSPZ.value = value.toUpperCase();   
 
     const result = value.replace(/\s/g, "").toUpperCase(); 
     
     // 1 
     if (spzFilter.test(result) == false) {
-        spzCode.style.display = "block";
+        licenceCode.style.display = "block";
         submitBtn.classList.add("disabled");
     } else {
-        spzCode.style.display = "none";
+        licenceCode.style.display = "none";
         submitBtn.classList.remove("disabled");
     }
 
     // 2
-    if (result.length != 7) {
+    if (result.length < 7 || result.length > 7) {
         e.currentTarget.classList.add("error");
-        spzInfo.style.display = "block";
+        licenceInfo.style.display = "block";
     } else {
         if (e.currentTarget.classList.contains("error")) {
             e.currentTarget.classList.remove("error"); 
-            spzInfo.style.display = "none";
+            licenceInfo.style.display = "none";
         }        
     }
 
@@ -47,13 +52,13 @@ spzInput.addEventListener("input", e => {
 
 submitBtn.addEventListener("click", e => {
     e.preventDefault();
-    if (!spzInput.classList.contains("error") && 
+    if (!licenceInput.classList.contains("error") && 
         !submitBtn.classList.contains("disabled")
     ) {
         msgBox.innerHTML = "Zadali jste validné číslo SPZ!";
-        spzInput.value = "";
+        licenceInput.value = "";
         shownSPZ.value = "";  
-        
+        licenceInfo.style.display = "none";        
     }
 })
 
